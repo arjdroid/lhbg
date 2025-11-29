e7646966c6135041d5e01632bc8fe36df8ae3777
Arjun Singh <57623600+Arjdroid@users.noreply.github.com>
2025-11-29 02:30:21 -0800

    03 ex 4 5

--- 02/hello.hs
+++ 02/hello.hs
@@ -3,5 +3,5 @@ html_ content = "<html>" <> content <> "</html>"
 body_ content = "<body>" <> content <> "</body>"
 head_ content = "<head>" <> content <> "</head>"
 title_ content = "<title>" <> content <> "</title>"
-myhtml = html_ (body_ "Hello, world!")
-
+myhtml = makeHtml "My page title" "My page content"
+makeHtml title content = html_ (head_ (title_ title) <> body_ content )
