742f2c0ec731e9cc4b74dfc8f82ab0629952238d
Arjun Singh <57623600+Arjdroid@users.noreply.github.com>
2025-11-29 04:03:16 -0800

    03 02 exercises

--- 02/hello.hs
+++ 02/hello.hs
@@ -1,3 +1,4 @@
+main :: IO()
 main = putStrLn myhtml
 
 html_ :: String -> String
@@ -12,11 +13,18 @@ head_ = el "head"
 title_ :: String -> String
 title_ = el "title"
 
-el :: String -> String -> String
-el tag content =
+p_ :: String -> String
+p_ = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
+
+el :: String -> (String -> String)
+el = \tag -> \content ->
   "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 
-myhtml = makeHtml "My page title" "My page content"
+myhtml :: String
+myhtml = makeHtml "My page title" "My page header" "My page paragraph"
 
-makeHtml :: String -> String -> String
-makeHtml title content = html_ (head_ (title_ title) <> body_ content)
+makeHtml :: String -> String -> String -> String
+makeHtml title header content = html_ (head_ (title_ title) <> h1_ header <> p_ content)
