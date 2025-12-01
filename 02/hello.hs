09f3c3b92cfc660610b3d84b005cfe9e7b2248ae
Arjun Singh <57623600+Arjdroid@users.noreply.github.com>
2025-12-01 03:51:09 -0800

    03 04 progress

--- 02/hello.hs
+++ 02/hello.hs
@@ -3,16 +3,12 @@ main = putStrLn myhtml
 
 myhtml :: String
 myhtml =
-  makeHtml
-    "Hello title"
-    (h1_ "Hello, world!" <> p_ "Let's learn about Haskell!")
-
-makeHtml :: String -> String -> String
-makeHtml title content = html_ (head_ (title_ title) <> body_ content)
+  "Hello title"
+  (h1_ "Hello, world!" <> p_ "Let's learn about Haskell!")
 
 newtype Html = Html String
 newtype Structure = Structure String
-newtype Title = Title String
+type Title = String
 
 append_ :: Structure -> Structure -> Structure
 append_ (Structure a) (Structure b) =
@@ -29,7 +25,17 @@ h1_ = Structure . el "h1"
 
 html_ :: Title -> Structure -> Html
 html_ title content =
-  Html . el "html" (el "head" (el "title" title <> el "body" content ))
+  Html
+    (
+      el "html"
+        (
+          el "head" (el "title" title)
+          <> el "body" (getStructureString content)
+        )
+    )
+
+getStructureString :: Structure -> String
+getStructureString (Structure a) = a
 
 el :: String -> (String -> String)
 el tag content =
