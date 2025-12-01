259760f985f5f53e1b1bd4bf37b4bab695d5bee0
Arjun Singh <57623600+Arjdroid@users.noreply.github.com>
2025-11-30 23:40:12 -0800

    parametric polymorphism is weird

--- 02/hello.hs
+++ 02/hello.hs
@@ -13,6 +13,13 @@ makeHtml title content = html_ (head_ (title_ title) <> body_ content)
 newtype Html = Html String
 newtype Structure = Structure String
 
+append_ :: Structure -> Structure -> Structure
+append_ (Structure a) (Structure b) =
+  Structure (a <> b)
+
+render :: Html -> String
+render (Html a) = a
+
 html_ :: String -> String
 html_ = el "html"
 
