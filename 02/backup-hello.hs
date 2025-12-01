9611c5b29c937b47aaeffcff5df0997e6e82cc85
Arjun Singh <57623600+Arjdroid@users.noreply.github.com>
2025-12-01 03:45:46 -0800

    03 04 progress

--- 02/backup-hello.hs
+++ 02/backup-hello.hs
@@ -12,7 +12,6 @@ makeHtml title content = html_ (head_ (title_ title) <> body_ content)
 
 newtype Html = Html String
 newtype Structure = Structure String
-newtype Title = Title String
 
 append_ :: Structure -> Structure -> Structure
 append_ (Structure a) (Structure b) =
@@ -21,15 +20,23 @@ append_ (Structure a) (Structure b) =
 render :: Html -> String
 render (Html a) = a
 
-p_ :: String -> Structure
-p_ = Structure . el "p"
+html_ :: String -> String
+html_ = el "html"
 
-h1_ :: String -> Structure
-h1_ = Structure . el "h1"
+body_ :: String -> String
+body_ = el "body"
 
-html_ :: Title -> Structure -> Html
-html_ title content =
-  Html . el "html" (el "head" (el "title" title <> el "body" content ))
+head_ :: String -> String
+head_ = el "head"
+
+title_ :: String -> String
+title_ = el "title"
+
+p_ :: String -> String
+p_ = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
 
 el :: String -> (String -> String)
 el tag content =
