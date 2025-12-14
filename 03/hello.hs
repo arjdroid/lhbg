452159ac483b5818f356aab1681847f23438b1ce
Arjun Singh <57623600+Arjdroid@users.noreply.github.com>
2025-12-13 16:32:09 -0800

    modularise html generation

---
+++ 03/hello.hs
@@ -0,0 +1,10 @@
+import Html
+
+main :: IO()
+main = putStrLn (render myhtml)
+
+myhtml :: Html
+myhtml =
+  html_
+    "Hello title" 
+    (append_ (h1_ "Hello, world!") (p_ "Let's learn about Haskell!"))
